a252e58519631ebbc7608244339e9761f02bb562
HippErger <berger.clayton@gmail.com>
2017-10-16 20:22:07 -0500

    refamiliarizing myself with CSS

--- mywebsite/src/App.js
+++ mywebsite/src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+// <img src={logo} className="App-logo" alt="logo" />
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+
         </header>
         <p className="App-intro">
-          Tell me sell me to you.
+          Let me sell me to you.
         </p>
         <footer>
           <div className= "Footer-div">
-            <h3 className="Footer-tiles" >Towers of Hanoi</h3>
-            <h3 className="Footer-tiles" >Job 2 </h3>
+            <h3 className="Footer-tiles" > Towers of Hanoi </h3>
+            <h3 className="Footer-tiles" > Job 2 </h3>
           </div>
           <div className= "Footer-div">
-            <h3 className="Footer-tiles" >Job 3 </h3>
-            <h3 className="Footer-tiles" >Job 4 </h3>
+            <h3 className="Footer-tiles" > Job 3 </h3>
+            <h3 className="Footer-tiles" > Job 4 </h3>
           </div>
         </footer>
       </div>
